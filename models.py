31d2337035ac326fd1fff8fee8ddd0a521226474
monicathieu <mthieu@emory.edu>
2023-08-30 13:41:58 -0400

    init emonet class code

---
+++ models.py
@@ -0,0 +1,69 @@
+# %%
+# imports per usual
+import torch
+from torch import nn
+
+# %%
+# Pythonic EmoNet (from Matlab-ONNX, minus technically unnecessary layers and grouped nicer)
+class EmoNet(nn.Module):
+    def __init__(self, num_classes: int = 20) -> None:
+        # This creates the classes that will come in from the onnx2torch dict
+        # Every parameter has to match for it to read in
+        # So we need stuff like the weight initializers, which I think don't actually matter for inference
+        super().__init__()
+        alexnet_lrn_alpha = 9.999999747378752e-05
+
+        # Kernel size is the size of the moving window in square px
+        # 3 channels in per pixel (RGB), 96 channels out per conv center (that's a lotta info!)
+        self.conv_0 = nn.Sequential(
+            nn.Conv2d(3, 96, kernel_size=11, stride=4),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=alexnet_lrn_alpha, beta=0.75, k=1),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False)
+        )
+        self.conv_1 = nn.Sequential(
+            nn.Conv2d(96, 256, kernel_size=5, stride=1, padding=2, groups=2),
+            nn.ReLU(),
+            nn.LocalResponseNorm(size=5, alpha=alexnet_lrn_alpha, beta=0.75, k=1),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False)
+        )
+        self.conv_2 = nn.Sequential(
+            nn.Conv2d(256, 384, kernel_size=3, stride=1, padding=1),
+            nn.ReLU()
+        )
+        self.conv_3 = nn.Sequential(
+            nn.Conv2d(384, 384, kernel_size=3, stride=1, padding=1, groups=2),
+            nn.ReLU()
+        )
+        self.conv_4 = nn.Sequential(
+            nn.Conv2d(384, 256, kernel_size=3, stride=1, padding=1, groups=2),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=0, dilation=1, ceil_mode=False)
+        )
+        self.conv_5 = nn.Sequential(
+            nn.Conv2d(256, 4096, kernel_size=6, stride=1),
+            nn.ReLU()
+        )
+        self.conv_6 = nn.Sequential(
+            nn.Conv2d(4096, 4096, kernel_size=1, stride=1),
+            nn.ReLU()
+        )
+        self.classifier = nn.Sequential(
+            nn.Conv2d(4096, num_classes, kernel_size=1, stride=1),
+            nn.Flatten(start_dim=-3, end_dim=-1), # flatten all except batch and class dims
+            nn.Softmax(dim=-1)
+        )
+    
+    def forward(self, x: torch.Tensor):
+        # This is the one that actually EXECUTES the model
+        x = x.to(torch.float)
+        x = self.conv_0(x)
+        x = self.conv_1(x)
+        x = self.conv_2(x)
+        x = self.conv_3(x)
+        x = self.conv_4(x)
+        x = self.conv_5(x)
+        x = self.conv_6(x)
+        x = self.classifier(x)
+
+        return x
